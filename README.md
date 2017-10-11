95c1d39bee2a3e9662020a4d853d2d4af79d18c1
Martial Lapeyre <lapeyre.martial@gmail.com>
2017-10-11 16:29:56 +0200

    Create 3 modules without deps

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mix-java-kotlin
 mix-java-kotlin
+A minimal project for testing "mixability" between Java and kotlin language
---
+++ mix/src/main/kotlin/App.kt
@@ -0,0 +1,7 @@
+public class App {
+    fun sayHello(name: String): String {
+        val pwet = Pwet()
+        return "Hello $name $pwet"
+    }
+}
+
